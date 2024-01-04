f439fdc7bd21eda07a4cf8bcfbd808268b7f7102
yuange <745517491@qq.com>
2024-01-04 18:52:43 +0800

    IDEA 代码注释模板设置

---
+++ java/MyPOITest/src/main/java/org/yg/codeTemplate/CodeTemplate2.java
@@ -0,0 +1,10 @@
+package org.yg.codeTemplate;
+
+/**
+ * @author yuange
+ * @version 1.0
+ * @description: TODO
+ * @date 2024-1-4 18:42
+ */
+public class CodeTemplate2 {
+}
---
+++ java/MyPOITest/src/main/java/org/yg/codeTemplate/CodeTemplateTest.java
@@ -0,0 +1,23 @@
+package org.yg.codeTemplate;
+
+/**
+ * @description: 测试类注释
+ * @author yuange
+ * @date 2024-1-4 17:09
+ * @version 1.0
+ */
+public class CodeTemplateTest {
+	public static void main(String[] args) {
+		test("hello world");
+	}
+	/**
+	 * @description: 测试方法注释
+	 * @param: msg
+	 * @return: java.lang.String
+	 * @author yuange
+	 * @date: 2024-1-4 17:07
+	 */
+	public static String test(String msg){
+		return msg;
+	}
+}
